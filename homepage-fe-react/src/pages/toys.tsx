d61058b134037dd85635e9ede47b6af948857bbd
seniya2 <seniya2@gmail.com>
2021-02-15 18:36:26 +0900

    feat: add toys

--- homepage-fe-react/src/pages/toys.tsx
+++ homepage-fe-react/src/pages/toys.tsx
@@ -7,8 +7,25 @@ function Toys() {
           <p>Simple Chat with React</p>
           <p>
             Demo :
-            <a href="http://www.malrang.net:8100/" target="_blank">
+            <a href="http://www.malrang.net:8100" target="_blank">
               http://www.malrang.net:8100/
+            </a>
+          </p>
+          <p>
+            Source :
+            <a href="https://github.com/seniya/toys-malrang/tree/main/simple-chat" target="_blank">
+              https://github.com/seniya/toys-malrang/tree/main/simple-chat
+            </a>
+          </p>
+        </div>
+
+        <hr />
+        <div>
+          <p>Simple Chat with Vue3</p>
+          <p>
+            Demo :
+            <a href="http://www.malrang.net:8101" target="_blank">
+              http://www.malrang.net:8101/
             </a>
           </p>
           <p>
